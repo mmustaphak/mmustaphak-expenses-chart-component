c757ba43b01ac782fdc9a99f341ded69d3391581
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2024-04-24 15:32:19 +0530

    Configured Color palette in tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'soft-red': 'hsl(var(--soft-red))',
+        'cyan': 'hsl(var(--cyan))',
+        'dark-brown': 'hsl(var(--dark-brown))',
+        'medium-brown': 'hsl(var(--medium-brown))',
+        'cream': 'hsl(var(--cream))',
+        'pale-orange': 'hsl(var(--pale-orange))',
+      }
+    },
   },
   plugins: [],
 }
