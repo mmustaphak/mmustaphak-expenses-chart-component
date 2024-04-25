7d48bd59ea194407349c6b3cf81689e6909cc5ef
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2024-04-25 12:04:33 +0530

    Added Main component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header.jsx"
+import Main from "./components/Main.jsx"
 
 export default function App() {
   return (
     <div className="w-[95%] mt-8 mx-auto">
       <Header/>
+      <Main/>
     </div>
   )
 }
---
+++ src/components/Main.jsx
@@ -0,0 +1,21 @@
+const Main = ()=>{
+    return(
+        <main>
+            <h2>Spending - Last 7 days</h2>
+            <div>
+
+            </div>
+            <hr />
+            <div>
+                <p><small>Total this month</small></p>
+                <strong>$478.33</strong>
+            </div>
+            <div>
+                <p><small>+2.4%</small></p>
+                <p>from last month</p>
+            </div>
+        </main>
+    )
+}
+
+export default Main
