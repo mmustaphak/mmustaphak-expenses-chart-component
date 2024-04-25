1ae4a4a07039a3bfa7a6d60dc94719a9b130a620
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2024-04-25 12:29:35 +0530

    Added basic styling to the main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,18 +1,20 @@
 const Main = ()=>{
     return(
-        <main>
-            <h2>Spending - Last 7 days</h2>
+        <main className="mt-4 py-4 px-4 rounded-md bg-white text-dark-brown">
+            <h2 className="font-bold text-xl">Spending - Last 7 days</h2>
             <div>
 
             </div>
-            <hr />
-            <div>
-                <p><small>Total this month</small></p>
-                <strong>$478.33</strong>
-            </div>
-            <div>
-                <p><small>+2.4%</small></p>
-                <p>from last month</p>
+            <hr className="border" />
+            <div className="flex justify-between">
+                <div>
+                    <p className="text-medium-brown"><small>Total this month</small></p>
+                    <strong className="text-2xl">$478.33</strong>
+                </div>
+                <div>
+                    <p className="font-bold text-end"><small>+2.4%</small></p>
+                    <p className="text-medium-brown">from last month</p>
+                </div>
             </div>
         </main>
     )
