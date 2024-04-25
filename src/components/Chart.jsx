208ce1a966131124a42c406fa0ebdfee444c7977
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2024-04-25 15:12:05 +0530

    Added Overall styling to Chart component

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,13 +1,36 @@
 const Chart = ()=>{
     return(
         <div className="flex justify-between mt-6">
-                <div className="min-w-[28px] min-h-32 rounded bg-soft-red"></div>
-                <div className="min-w-[28px] min-h-32 rounded bg-soft-red"></div>
-                <div className="min-w-[28px] min-h-32 rounded bg-cyan"></div>
-                <div className="min-w-[28px] min-h-32 rounded bg-soft-red"></div>
-                <div className="min-w-[28px] min-h-32 rounded bg-soft-red"></div>
-                <div className="min-w-[28px] min-h-32 rounded bg-soft-red"></div>
-                <div className="min-w-[28px] min-h-32 rounded bg-soft-red"></div>
+                <div>
+                    <div className="min-w-[28px] min-h-32 rounded bg-soft-red"></div>
+                    <p className="mt-2 text-medium-brown text-xs">Mon</p>
+                </div>
+                <div>
+                    <div className="min-w-[28px] min-h-32 rounded bg-soft-red"></div>
+                    <p className="mt-2 text-medium-brown text-xs">Mon</p>
+                </div>
+                <div>
+                    <div className="min-w-[28px] min-h-32 rounded bg-cyan"></div>
+                    <p className="mt-2 text-medium-brown text-xs">Mon</p>
+                </div>
+                <div>
+                    <div className="min-w-[28px] min-h-32 rounded bg-soft-red"></div>
+                    <p className="mt-2 text-medium-brown text-xs">Mon</p>
+                </div>
+                <div>
+
+                    <div className="min-w-[28px] min-h-32 rounded bg-soft-red"></div>
+                    <p className="mt-2 text-medium-brown text-xs">Mon</p>
+                </div>
+                <div>
+
+                    <div className="min-w-[28px] min-h-32 rounded bg-soft-red"></div>
+                    <p className="mt-2 text-medium-brown text-xs">Mon</p>
+                </div>
+                <div>
+                    <div className="min-w-[28px] min-h-32 rounded bg-soft-red"></div>
+                    <p className="mt-2 text-medium-brown text-xs">Mon</p>
+                </div>
         </div>
     )
 }
