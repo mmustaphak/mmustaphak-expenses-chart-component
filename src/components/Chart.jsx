7d87415fa3c7ef2bd0692c4ebcd16b0d8d332555
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2024-04-25 12:41:56 +0530

    Created and imported Chart component into Main component

---
+++ src/components/Chart.jsx
@@ -0,0 +1,15 @@
+const Chart = ()=>{
+    return(
+        <div className="flex justify-between mt-6">
+                <div className="min-w-[28px] min-h-32 rounded bg-soft-red"></div>
+                <div className="min-w-[28px] min-h-32 rounded bg-soft-red"></div>
+                <div className="min-w-[28px] min-h-32 rounded bg-cyan"></div>
+                <div className="min-w-[28px] min-h-32 rounded bg-soft-red"></div>
+                <div className="min-w-[28px] min-h-32 rounded bg-soft-red"></div>
+                <div className="min-w-[28px] min-h-32 rounded bg-soft-red"></div>
+                <div className="min-w-[28px] min-h-32 rounded bg-soft-red"></div>
+        </div>
+    )
+}
+
+export default Chart
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,12 @@
+import Chart from "./Chart.jsx"
+
 const Main = ()=>{
     return(
         <main className="mt-4 py-4 px-4 rounded-md bg-white text-dark-brown">
             <h2 className="font-bold text-xl">Spending - Last 7 days</h2>
-            <div>
-
-            </div>
-            <hr className="border" />
-            <div className="flex justify-between">
+            <Chart/>
+            <hr className="border mt-4" />
+            <div className="flex justify-between mt-4">
                 <div>
                     <p className="text-medium-brown"><small>Total this month</small></p>
                     <strong className="text-2xl">$478.33</strong>
