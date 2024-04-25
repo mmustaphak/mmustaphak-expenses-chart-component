fa6d41311996721414bb66d77cfe94a7157d49be
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2024-04-25 11:55:43 +0530

    Added basic styling to header component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import Header from "./components/Header.jsx"
 
 export default function App() {
   return (
-    <Header/>
+    <div className="w-[95%] mt-8 mx-auto">
+      <Header/>
+    </div>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
 import logo from "../assets/logo.svg"
 const Header = ()=>{
     return(
-        <header>
+        <header className="flex justify-between items-center min-w-[305px] px-3 py-4 rounded-md bg-soft-red text-white">
             <div>
-                <p>My balance</p>
-                <strong>$921.48</strong>
+                <p className="text-xs font-light">My balance</p>
+                <strong className="text-lg">$921.48</strong>
             </div>
             <img src={logo} alt="Company logo" />
         </header>
