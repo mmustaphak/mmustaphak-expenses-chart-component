7c00f387db9bfb28418260ff83e1b5a09a17f7b1
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2024-04-25 15:19:47 +0530

    Mappd over data.json in Chart.jsx

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,36 +1,17 @@
+import data from "../data.json"
+
+const renderedChartData = data.map((expenses)=>{
+    return(
+        <div key={expenses.day}>
+            <div className="min-w-[28px] min-h-32 rounded bg-soft-red"></div>
+            <p className="mt-2 text-medium-brown text-xs">{expenses.day}</p>
+        </div>
+    )
+})
 const Chart = ()=>{
     return(
         <div className="flex justify-between mt-6">
-                <div>
-                    <div className="min-w-[28px] min-h-32 rounded bg-soft-red"></div>
-                    <p className="mt-2 text-medium-brown text-xs">Mon</p>
-                </div>
-                <div>
-                    <div className="min-w-[28px] min-h-32 rounded bg-soft-red"></div>
-                    <p className="mt-2 text-medium-brown text-xs">Mon</p>
-                </div>
-                <div>
-                    <div className="min-w-[28px] min-h-32 rounded bg-cyan"></div>
-                    <p className="mt-2 text-medium-brown text-xs">Mon</p>
-                </div>
-                <div>
-                    <div className="min-w-[28px] min-h-32 rounded bg-soft-red"></div>
-                    <p className="mt-2 text-medium-brown text-xs">Mon</p>
-                </div>
-                <div>
-
-                    <div className="min-w-[28px] min-h-32 rounded bg-soft-red"></div>
-                    <p className="mt-2 text-medium-brown text-xs">Mon</p>
-                </div>
-                <div>
-
-                    <div className="min-w-[28px] min-h-32 rounded bg-soft-red"></div>
-                    <p className="mt-2 text-medium-brown text-xs">Mon</p>
-                </div>
-                <div>
-                    <div className="min-w-[28px] min-h-32 rounded bg-soft-red"></div>
-                    <p className="mt-2 text-medium-brown text-xs">Mon</p>
-                </div>
+            {renderedChartData}
         </div>
     )
 }
