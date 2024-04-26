814b0a826ae22a8d65550c8fbb5f937a175c94ea
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2024-04-26 10:52:55 +0530

    Made Bars to dynamically changed based on the amount property in data.json

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,16 +1,25 @@
+import { useState } from "react"
 import data from "../data.json"
 
-const renderedChartData = data.map((expenses)=>{
-    return(
-        <div key={expenses.day}>
-            <div className="min-w-[28px] min-h-32 rounded bg-soft-red"></div>
-            <p className="mt-2 text-medium-brown text-xs">{expenses.day}</p>
-        </div>
-    )
-})
-const Chart = ()=>{
-    return(
-        <div className="flex justify-between mt-6">
+
+const Chart = () => {
+
+    const [chartHeight, setChartHeight] = useState()
+
+    const renderedChartData = data.map((expenses) => {
+        const percentageSizeofBar = (expenses.amount * 100)/chartHeight
+        //Formula to get the value in px and multiplied by 2.5 to scale the bars
+        const barHeight = ((160 * percentageSizeofBar)/100)*2.5
+        return (
+            <div key={expenses.day}>
+                <div style={{height: barHeight}} className="min-w-[28px] rounded bg-soft-red"></div>
+                <p className="mt-2 text-medium-brown text-xs">{expenses.day}</p>
+            </div>
+        )
+    })
+
+    return (
+        <div className="flex justify-between items-end mt-6 min-h-40" ref={node => setChartHeight(node?.clientHeight)}>
             {renderedChartData}
         </div>
     )
