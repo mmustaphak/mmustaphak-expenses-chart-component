11122f2dddd7aaaa62d8b214a77b6b0bdea62a7b
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2024-04-27 14:36:27 +0530

    Add hover features to the chart component

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -9,11 +9,13 @@ const Chart = () => {
     const renderedChartData = data.map((expenses) => {
         const percentageSizeofBar = (expenses.amount * 100)/chartHeight
         //Formula to get the value in px and multiplied by 2.5 to scale the bars
-        const barHeight = ((160 * percentageSizeofBar)/100)*2.5
+        const barHeight = ((160 * percentageSizeofBar)/100)*2
         const barColor = expenses.day == "wed" ? "bg-cyan" : "bg-soft-red"
         return (
-            <div key={expenses.day}>
-                <div style={{height: barHeight}} className={`min-w-[28px] rounded ${barColor}`}></div>
+            <div key={expenses.day} className="relative flex flex-col items-center">
+                <div style={{height: barHeight}} className={`peer min-w-[30px] rounded ${barColor} hover:opacity-75`}></div>
+                {/*This is here because the peer marker can only be used on the previous sibling */}
+                <p className="absolute -top-7 p-1 invisible rounded-sm text-xs font-semibold text-white bg-dark-brown peer-hover:visible">${expenses.amount}</p>
                 <p className="mt-2 text-medium-brown text-xs">{expenses.day}</p>
             </div>
         )
